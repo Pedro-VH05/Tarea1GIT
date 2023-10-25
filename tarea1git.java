ae98637233bb81d00deb02b0ad127922bde64967
Pedro <pedrovigara12@gmail.com>
2023-10-25 10:00:22 +0200

    Modificacion Java

--- tarea1git.java
+++ tarea1git.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 class tarea1git {
 	public static void main(String[] Args) {
-		
-		System.out.println("Hola Mundo");
-  }
-	
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Escribe el primer número: ");
+		double primerNumero = Double.parseDouble(sc.nextLine());
+		System.out.println("Escribe el segundo número: ");
+		double segundoNumero = Double.parseDouble(sc.nextLine());
+
+		double resultado = primerNumero + segundoNumero;
+		System.out.println("La suma es " + resultado);
+	}
 }
